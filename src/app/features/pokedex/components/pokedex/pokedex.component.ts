dfca1825285436e167a39011a05512d57677eb7d
Aitor Lancharro <aitor.lancharro@gmail.com>
2023-04-29 14:10:30 +0200

    pokemon-services fixed #20

--- src/app/features/pokedex/components/pokedex/pokedex.component.ts
+++ src/app/features/pokedex/components/pokedex/pokedex.component.ts
@@ -1,10 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Species } from "src/app/shared/interfaces/schema";
+import { GetPokemonListService } from "src/app/shared/services/get-pokemon-list.service";
 
 @Component({
   selector: "app-pokedex",
   templateUrl: "./pokedex.component.html",
-  styleUrls: ["./pokedex.component.scss"]
+  styleUrls: ["./pokedex.component.scss"],
 })
-export class PokedexComponent {
+export class PokedexComponent implements OnInit {
+  pokemon$!: Observable<Species[]>;
 
+  constructor(private getPokemonList: GetPokemonListService) {}
+
+  ngOnInit(): void {
+    this.pokemon$ = this.getPokemonList
+      .watch({ limit: 1008, offset: 0 })
+      .valueChanges.pipe(map((result) => result.data.species));
+  }
 }
