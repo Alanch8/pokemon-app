4a12d9c704cc877faaf061106312401bf66225c0
Aitor Lancharro <aitor.lancharro@gmail.com>
2023-05-01 00:17:00 +0200

    footer start

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from "@angular/common";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./components/home/home.component";
 import { CarouselComponent } from "./components/carousel/carousel.component";
+import { CoreModule } from "src/app/core/core.module";
 
 @NgModule({
   declarations: [
     HomeComponent,
     CarouselComponent
   ],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, CoreModule],
 })
 export class HomeModule {}
