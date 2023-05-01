9fc052c9c509c51b532aab5ddd5f3a30ae0518f7
sergiJimenez <acasualcolumbus@gmail.com>
2023-05-01 19:11:37 +0200

    Load more finished fixed #29

--- src/app/features/pokedex/pokedex.module.ts
+++ src/app/features/pokedex/pokedex.module.ts
@@ -5,9 +5,17 @@ import { PokedexRoutingModule } from "./pokedex-routing.module";
 import { PokemonComponent } from "./components/pokemon/pokemon.component";
 import { PokedexComponent } from "./components/pokedex/pokedex.component";
 import { ReactiveFormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
 
 @NgModule({
   declarations: [PokemonComponent, PokedexComponent],
-  imports: [CommonModule, PokedexRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    PokedexRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    InfiniteScrollModule,
+  ],
 })
 export class PokedexModule {}
